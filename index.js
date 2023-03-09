65c1fe75c0ef8dffca140c1d7984e6df73618607
kSaigusa <122540392+kSaigusa@users.noreply.github.com>
2023-03-09 12:15:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ $(function () {
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var name = $('input[name="name"]:checked').val();
-        var number = $('input[name="phone-number"]:checked').val();
+        var name = $('input[name="name"]').val();
+        var number = $('input[name="phone-number"]').val();
         var item = $("#select").children("option:selected");
         var itemText = item.text();
-        var inumber = $('input[name="items-number"]:checked').val();
+        var inumber = $('input[name="items-number"]').val();
 
         var msg = `受け取り日：${date}\n氏名：${name}\n電話番号：${number}\n注文商品：${itemText}\n個数：${inumber}`;
         sendText(msg);
