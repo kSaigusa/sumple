b922010cede36ba261f9592afb999f87c8d24523
kSaigusa <122540392+kSaigusa@users.noreply.github.com>
2023-03-09 15:13:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 $(function () {
     // カレンダー
     $(function () {
-        $('input[name="date"]').datepicker({
-            dateFormat: 'yy/mm/dd',
+        $('input[name="date"]').datetimepicker({
+            step:15,
         });
     });
 
     // 送信
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
-        var name = $('input[name="name"]').val();
+        var name = document.getElementById("form-name").value ;
         var number = $('input[name="phone-number"]').val();
         var item = $("#select").children("option:selected");
         var itemText = item.text();
         var inumber = $('input[name="items-number"]').val();
 
-        var msg = `受け取り日：${date}\n氏名：${name}\n電話番号：${number}\n注文商品：${itemText}\n個数：${inumber}`;
+        var msg = `受け取り日時：${date}\n氏名：${name}\n電話番号：${number}\n注文商品：${itemText}\n個数：${inumber}`;
         sendText(msg);
 
         return false;
