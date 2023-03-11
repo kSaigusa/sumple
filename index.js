2e912ea69121535aaefdc83a171d3f01badde07d
kSaigusa <122540392+kSaigusa@users.noreply.github.com>
2023-03-11 15:57:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,13 +21,14 @@ $(function () {
     $('form').submit(function () {
         var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
+        var phone = $('input[name="phone-number"]')val();
         var names = '';
         $('#form-name').children().each(function (i, elm) {
             names += $(elm).val() + '、';
         })
         names = names.slice(0, -1);
-
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        
+        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}\n電話番号：${phone}`;
         sendText(msg);
 
         return false;
